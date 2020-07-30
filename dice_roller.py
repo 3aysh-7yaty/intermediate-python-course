630d5a32131253660fbc18a155332f0648c61bf7
3aysh-7yati <abdellh1@hotmail.com>
2020-07-30 17:40:44 +0300

    changes

---
+++ dice_roller.py
@@ -0,0 +1,9 @@
+def main():
+  import random
+  dice_rolls = 2
+  for i in range(0, dice_rolls):
+    roll = random.randint(1,6)
+    print(f'You rolled a {roll}')
+
+if __name__== "__main__":
+  main()
